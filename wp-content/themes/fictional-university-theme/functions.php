3cdaa223a9cac17d98e70feff7143998fe31bbed
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-30 10:47:06 +0200

    Avoiding cache (CSS & Js) during development

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,11 +1,16 @@
 <?php
 
+function getCacheVersion(){
+    // return "1.0"; // for production
+    return microtime();// for development
+}
+
 function university_files()
 {
-    wp_enqueue_script('university-main-scripts', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
+    wp_enqueue_script('university-main-scripts', get_theme_file_uri('/js/scripts-bundled.js'), null, getCacheVersion(), true);
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_style', get_stylesheet_uri());
+    wp_enqueue_style('university_main_style', get_stylesheet_uri(), null, getCacheVersion());
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
