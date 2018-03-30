184d7a8a7178f62edeb6dc9f4f7ab7446935b74b
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-30 17:46:57 +0200

    Blog continued

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -23,7 +23,7 @@ while (have_posts()) {
     <div class="post-item">
       <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
       <div class="metabox">
-        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('D d,F Y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+        <p>Posted by <?php the_author_posts_link();?> on <?php the_time('D d,F Y');?> in <?php echo get_the_category_list(', '); ?></p>
       </div>
       <div class="generic-content">
         <?php the_excerpt();?>
@@ -32,6 +32,8 @@ while (have_posts()) {
     </div>
   <?php
 }
+
+echo paginate_links();
 
 ?>
 
