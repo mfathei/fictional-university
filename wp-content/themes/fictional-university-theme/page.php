740dc55b3f48a7bc1920324e2344fe51195c8a47
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-30 13:17:05 +0200

    Parent & Children Pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -4,25 +4,40 @@ get_header();
 while (have_posts()) {
     the_post();
     ?>
-        
+
 
 
   <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <h1 class="page-banner__title"><?php the_title();?></h1>
       <div class="page-banner__intro">
         <p>REMEMBER TO REPLACE ME LATER.</p>
       </div>
-    </div>  
+    </div>
   </div>
 
   <div class="container container--narrow page-section">
 
+<?php
+
+    $theParent = wp_get_post_parent_ID(get_the_ID());
+
+    if ($theParent) {
+        ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p>
+        <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>">
+        <i class="fa fa-home" aria-hidden="true"></i> 
+        Back to <?php echo get_the_title($theParent); ?></a> 
+        <span class="metabox__main"><?php the_title();?></span>
+      </p>
     </div>
-<!--     
+   <?php
+}
+?>
+
+<!--
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
@@ -32,7 +47,7 @@ while (have_posts()) {
     </div> -->
 
     <div class="generic-content">
-      <?php the_content(); ?>
+      <?php the_content();?>
     </div>
 
   </div>
@@ -41,7 +56,6 @@ while (have_posts()) {
 
     <?php
 }
-
 
 get_footer();
 ?>
