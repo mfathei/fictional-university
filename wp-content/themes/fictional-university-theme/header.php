599be88cae4c5f19c68d42e145f6a32a3357c17e
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-30 14:24:43 +0200

    Few quick edits

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class(); ?> >
 
   <header class="site-header">
     <div class="container">
