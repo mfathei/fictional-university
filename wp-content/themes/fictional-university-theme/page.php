27069a351fa207965b56976a4ed05996a752fa14
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-30 14:06:19 +0200

    Menu of Child Pages Links

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -28,23 +28,49 @@ while (have_posts()) {
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p>
         <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>">
-        <i class="fa fa-home" aria-hidden="true"></i> 
-        Back to <?php echo get_the_title($theParent); ?></a> 
+        <i class="fa fa-home" aria-hidden="true"></i>
+        Back to <?php echo get_the_title($theParent); ?></a>
         <span class="metabox__main"><?php the_title();?></span>
       </p>
     </div>
    <?php
 }
-?>
+    ?>
 
-<!--
+
+<?php
+
+    $childsArray = get_pages(array(
+        'child_of' => get_the_ID(),
+    ));
+    if ($theParent or $childsArray) {
+
+        ?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title">
+        <a href="<?php echo get_permalink($theParent); ?>">
+          <?php echo get_the_title($theParent); ?>
+        </a>
+      </h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+      <?php
+
+        $childsOf = 0;
+        if ($theParent) {
+            $childsOf = $theParent;
+        } else {
+            $childsOf = get_the_ID();
+        }
+        wp_list_pages(array(
+            'title_li' => null,
+            'child_of' => $childsOf,
+            'sort_column' => 'menu_order',
+        ));
+        ?>
       </ul>
-    </div> -->
+    </div>
+
+  <?php }?>
 
     <div class="generic-content">
       <?php the_content();?>
