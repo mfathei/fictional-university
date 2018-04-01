764fb57ff7fa7be7b6fc12447e4bec2868af52f1
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-01 20:24:33 +0200

    Create Your Own Raw JSON Data

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -2,13 +2,29 @@
 
 add_action('rest_api_init', 'universityRegisterSearch');
 
-function universityRegisterSearch(){
+function universityRegisterSearch()
+{
     register_rest_route('university/v1', 'search', array(
-        'methods'  => WP_REST_SERVER::READABLE,
-        'callback' => 'universitySearchResults'
+        'methods' => WP_REST_SERVER::READABLE,
+        'callback' => 'universitySearchResults',
     ));
 }
 
-function universitySearchResults(){
-    return 'Congratulations, you created your first rest route.';
+function universitySearchResults()
+{
+    $professors = new WP_Query(array(
+        'post_type' => 'professor'
+    ));
+
+    $searchResults = array();
+
+    while($professors->have_posts()){
+        $professors->the_post();
+        array_push($searchResults, array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+        ));
+    }
+
+    return $searchResults;
 }
