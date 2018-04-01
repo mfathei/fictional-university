f5b0bcab4c9494b146f7694115d151b40fb0e25e
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-01 10:52:42 +0200

    Open and Close search overlay

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -1,7 +1,26 @@
+import $ from 'jquery';
+
 class Search {
     constructor() {
-        alert("Hello, I am a search.");
+        this.openButton = $(".js-search-trigger");
+        this.closeButton = $(".search-overlay__close");
+        this.searchOverlay = $(".search-overlay");
+        this.events();
     }
+
+    events() {
+        this.openButton.on("click", this.openOverlay.bind(this));// .bind(this) will send 'this' object to the function
+        this.closeButton.on("click", this.closeOverlay.bind(this));
+    }
+
+    openOverlay() {
+        this.searchOverlay.addClass("search-overlay--active");
+    }
+
+    closeOverlay() {
+        this.searchOverlay.removeClass("search-overlay--active");
+    }
+
 }
 
 export default Search;
--- wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -13597,13 +13597,49 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _jquery = _interopRequireDefault(__webpack_require__(0));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Search = function Search() {
-  _classCallCheck(this, Search);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-  alert("Hello, I am a search.");
-};
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Search =
+/*#__PURE__*/
+function () {
+  function Search() {
+    _classCallCheck(this, Search);
+
+    this.openButton = (0, _jquery.default)(".js-search-trigger");
+    this.closeButton = (0, _jquery.default)(".search-overlay__close");
+    this.searchOverlay = (0, _jquery.default)(".search-overlay");
+    this.events();
+  }
+
+  _createClass(Search, [{
+    key: "events",
+    value: function events() {
+      this.openButton.on("click", this.openOverlay.bind(this)); // .bind(this) will send 'this' object to the function
+
+      this.closeButton.on("click", this.closeOverlay.bind(this));
+    }
+  }, {
+    key: "openOverlay",
+    value: function openOverlay() {
+      this.searchOverlay.addClass("search-overlay--active");
+    }
+  }, {
+    key: "closeOverlay",
+    value: function closeOverlay() {
+      this.searchOverlay.removeClass("search-overlay--active");
+    }
+  }]);
+
+  return Search;
+}();
 
 var _default = Search;
 exports.default = _default;
