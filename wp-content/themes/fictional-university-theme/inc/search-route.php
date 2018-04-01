3afd30c102ab20e1a3908278eafe00ad6001c0be
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-01 21:05:21 +0200

    Working With Multiple Post Types

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -12,20 +12,51 @@ function universityRegisterSearch()
 
 function universitySearchResults($data)
 {
-    $professors = new WP_Query(array(
-        'post_type' => 'professor',
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post', 'professor', 'page', 'event', 'program', 'campus'),
         's'         => sanitize_text_field($data['term'])
     ));
 
-    $searchResults = array();
+    $results = array(
+        'generalInformation' => array(),
+        'professors'        => array(),
+        'events'            => array(),
+        'programs'          => array(),
+        'campuses'          => array()
+    );
 
-    while($professors->have_posts()){
-        $professors->the_post();
-        array_push($searchResults, array(
-            'title' => get_the_title(),
-            'permalink' => get_the_permalink()
-        ));
+    while($mainQuery->have_posts()){
+        $mainQuery->the_post();
+        $type = get_post_type();
+
+        if($type == 'post' || $type == 'page'){
+            array_push($results['generalInformation'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        } else if($type == 'professor'){
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        } else if($type == 'program'){
+            array_push($results['programs'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        } else if($type == 'event'){
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        } else if($type == 'campus'){
+            array_push($results['campuses'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+        
     }
 
-    return $searchResults;
+    return $results;
 }
