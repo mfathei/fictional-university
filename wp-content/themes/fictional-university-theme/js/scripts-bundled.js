829a26a2adec234aa824e706a3cbea358a34c420
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-02 17:30:10 +0200

    Delete Posts with the REST API

--- wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -13762,18 +13762,22 @@ function () {
     key: "events",
     value: function events() {
       (0, _jquery.default)(".delete-note").on("click", this.deleteNote);
+      (0, _jquery.default)(".edit-note").on("click", this.editNote);
     } // Methods
 
   }, {
     key: "deleteNote",
-    value: function deleteNote() {
+    value: function deleteNote(e) {
+      var thisNote = (0, _jquery.default)(e.target).parents("li");
+
       _jquery.default.ajax({
         beforeSend: function beforeSend(xhr) {
           xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
         },
-        url: universityData.root_url + '/wp-json/wp/v2/note/85',
+        url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data("id"),
         type: 'DELETE',
         success: function success(response) {
+          thisNote.slideUp();
           console.log('Congrats');
           console.log(response);
         },
@@ -13782,6 +13786,13 @@ function () {
           console.log(_error);
         }
       });
+    }
+  }, {
+    key: "editNote",
+    value: function editNote(e) {
+      var thisNote = (0, _jquery.default)(e.target).parents("li");
+      thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+      thisNote.find(".update-note").addClass("update-note--visible");
     }
   }]);
 
