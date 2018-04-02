7c2ee39cb6fdde2d4ace7c9c265969aa9153a2b3
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-02 19:41:58 +0200

    Creating New Notes part2

--- wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -13761,9 +13761,9 @@ function () {
   _createClass(MyNotes, [{
     key: "events",
     value: function events() {
-      (0, _jquery.default)(".delete-note").on("click", this.deleteNote.bind(this));
-      (0, _jquery.default)(".edit-note").on("click", this.editNote.bind(this));
-      (0, _jquery.default)(".update-note").on("click", this.updateNote.bind(this));
+      (0, _jquery.default)("#my-notes").on("click", ".delete-note", this.deleteNote.bind(this));
+      (0, _jquery.default)("#my-notes").on("click", ".edit-note", this.editNote.bind(this));
+      (0, _jquery.default)("#my-notes").on("click", ".update-note", this.updateNote.bind(this));
       (0, _jquery.default)(".submit-note").on("click", this.createNote.bind(this));
     } // Methods
 
@@ -13837,7 +13837,7 @@ function () {
         data: ourNewNote,
         success: function success(response) {
           (0, _jquery.default)(".new-note-title, .new-note-body").val('');
-          (0, _jquery.default)('<li>Imagine real data here</li>').prependTo("#my-notes").hide().slideDown();
+          (0, _jquery.default)("\n                <li data-id=\"".concat(response.id, "\">\n                    <input readonly class=\"note-title-field\" type=\"text\" value=\"").concat(response.title.raw, "\">\n                    <span class=\"edit-note\"><i class=\"fa fa-pencil\" aria-hidden=\"true\"></i>Edit</span>\n                    <span class=\"delete-note\"><i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i>Delete</span>\n                    <textarea readonly class=\"note-body-field\">").concat(response.content.raw, "</textarea>\n                    <span class=\"update-note btn btn--blue btn--small\"><i class=\"fa fa-arrow-right\" aria-hidden=\"true\"></i>Save</span>\n                </li>\n                ")).prependTo("#my-notes").hide().slideDown();
           console.log('Congrats');
           console.log(response);
         },
