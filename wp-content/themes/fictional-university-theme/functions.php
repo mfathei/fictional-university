f0c13f128170c3b476357e95afa8cbb02feb689b
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-03 00:22:05 +0200

    Notes permissions and security part2

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -164,8 +164,15 @@ function ourHeaderTitle(){
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data){
-    if($data['post_type'] == 'note' && $data['post_status'] != 'trash'){
-        $data['post_status'] = "private";
+    if($data['post_type'] == 'note'){
+
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+
+        if($data['post_status'] != 'trash'){
+            $data['post_status'] = "private";
+        }
+        
     }
     return $data;
 }
