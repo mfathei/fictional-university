748e764ac075eec893aecdfac510df6e26a6bb33
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-02 09:48:19 +0200

    Traditional WordPress Searching part2

---
+++ wp-content/themes/fictional-university-theme/searchform.php
@@ -0,0 +1,9 @@
+    <div class="generic-content">
+        <form class="search-form" action="<?php echo esc_url(site_url('/')); ?>" method="get">
+            <label for="s" class="headline headline--medium">Perform a New Search:</label>
+            <div class="search-form-row">
+                <input class="s" type="text" name="s" id="s" placeholder="What are you looking for?">
+                <input class="search-submit" type="submit" value="Search">
+            </div>
+        </form>
+    </div>
