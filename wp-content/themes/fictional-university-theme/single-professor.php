241e482cc8655234cff1f613ca51855beba69e8b
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-03 11:39:23 +0200

    Let users like content

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -8,17 +8,54 @@ while (have_posts()) {
 ?>
   <div class="container container--narrow page-section">
 
+  <div class="generic-content">
+      <div class="row group">
+          <div class="one-third">
+              <img src="<?php the_post_thumbnail_url('professorPortrait'); ?>" alt="">
+          </div>
 
-    <div class="row group">
-        <div class="one-third">
-            <img src="<?php the_post_thumbnail_url('professorPortrait'); ?>" alt="">
-        </div>
+          <?php 
+            $likesCount = new WP_Query(array(
+              'post_type' => 'like',
+              'meta_query' => array(
+                array(
+                  'key' => 'liked_professor_id',
+                  'compare' => '=',
+                  'value' => get_the_ID()
+                )
+              )
+            ));
 
-        <div class="two-thirds">
-            <?php the_content(); ?>
-        </div>
-    </div>
+            $alreadyLiked = new WP_Query(array(
+              'author' => get_current_user_id(),
+              'post_type' => 'like',
+              'meta_query' => array(
+                array(
+                  'key' => 'liked_professor_id',
+                  'compare' => '=',
+                  'value' => get_the_ID()
+                )
+              )
+            ));
 
+            $likedIt = "no";
+
+            if($alreadyLiked->found_posts){
+              $likedIt = "yes";
+            }
+
+          ?>
+
+          <div class="two-thirds">
+              <span class="like-box" data-exists="<?php echo $likedIt; ?>">
+                <i class="fa fa-heart-o" area-hidden="true"></i>
+                <i class="fa fa-heart" area-hidden="true"></i>
+                <span class="like-count"><?php echo $likesCount->found_posts; ?></span>
+              </span>
+              <?php the_content(); ?>
+          </div>
+      </div>
+  </div>
     <?php
     
       $relatedPrograms = get_field('related_programs');
